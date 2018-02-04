04c250c5169c7ec2240bcea9f214c6d93e4c8bf1
Zer0-M <mjamil90@stuy.edu>
2018-02-04 01:45:35 -0500

    sqrt mostly working(not exact sqrt values but close enough); improved test for sqrt; added basic fib function but still working on possible improved algorithms

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Recursion{
   public int fact(int n){
     if(n<0){
@@ -11,11 +12,35 @@ public class Recursion{
   public int fib(int n){
     return 0;
   }
+  public int fibHelp(int k){
+    if(k<2){
+      return k;
+    }
+    return fibHelp(k-2)+fibHelp(k-1);
+  }
+
   public double sqrt(double n){
-    return 0;
+     if(n<0){
+      throw new IllegalArgumentException();
+    }
+    double guess=2;
+    return sqrtHelper(n,guess);
+  }
+  public boolean closeEnough(double n,double other){
+    return Math.abs((((n-other)/other)*100))<=0.00000000000005;
+  }
+  public double sqrtHelper(double n, double  guess){
+    if(n==0){
+      return 0;
+    }
+    if(closeEnough(guess*guess,n)){
+      return guess;
+    }
+    guess =  (n/guess+guess)/2.0;
+    return sqrtHelper(n,guess);
   }
   public static void main(String[] arg){
-    Recursion recur=new Recursion();
+    Recursion recur=new Recursion();/*
     //tests for factorial
     System.out.println(recur.fact(0));//expecting 1
     //System.out.println(recur.fact(-1));//expecting exception
@@ -25,16 +50,31 @@ public class Recursion{
 
     //tests for fib
     System.out.println(recur.fib(0));// expecting 0
-    System.out.println(recur.fib(-4));// expecting exception
-    System.out.println(recur.fib(4));// expecting 3
-    System.out.println(recur.fib(30));//expecting 832040
+    //System.out.println(recur.fib(-4));// expecting exception
+    for(int n=0;n<50;n++){
+      long startTime   = System.currentTimeMillis();
+      System.out.println(recur.fibHelp(n));// expecting 3
+      long endTime   = System.currentTimeMillis();
+      long totalTime = endTime - startTime;
+      System.out.println(totalTime);
+    }
 
+    System.out.println(recur.fib(5));//expecting 832040
+    */
     //tests for sqrt
-    System.out.println(recur.sqrt(0));//expecting 0.0
-    System.out.println(recur.sqrt(-4));//expecting exception
-    System.out.println(recur.sqrt(1));//expecting 1.0
-    System.out.println(recur.sqrt(2));//expecting 1.4
-    System.out.println(recur.sqrt(196));//expecting 14.0
-    System.out.println(recur.sqrt(236));//expecting 15.3
+    for(double d=0.0;d<10000;d+=1.0){
+      if(Math.sqrt(d)!=recur.sqrt(d)){
+        System.out.println(recur.closeEnough(Math.sqrt(d),recur.sqrt(d)));
+        System.out.println(Math.sqrt(d)+" vs. "+recur.sqrt(d));
+      }
+    }
+    System.out.println("\nPerfect Squares");
+    for(double n=0.0;n<100;n+=1.0){
+      double d=n*n;
+      if(Math.sqrt(d)!=recur.sqrt(d)){
+        System.out.println(recur.closeEnough(Math.sqrt(d),recur.sqrt(d)));
+        System.out.println(Math.sqrt(d)+" vs. "+recur.sqrt(d));
+      }
+    }
   }
 }
