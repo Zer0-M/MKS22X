a877967174eb51f3937f667968a0682b06b21219
Zer0-M <msj2340@gmail.com>
2018-02-09 14:17:54 -0500

    added fields, constructor and method signatures

---
+++ 02NQueens/QueenBoard.java
@@ -0,0 +1,35 @@
+public class QueenBoard{
+    private int[][]board;
+    public QueenBoard(int size){}
+    /**
+     *@return The output string formatted as follows:
+     *All numbers that represent queens are replaced with 'Q' 
+     *all others are displayed as underscores '_'
+     *There are spaces between each symbol:  
+     *Q _ _ _
+     *_ _ Q _
+     * etc.
+     */
+    public String toString(){
+	return "";
+    }
+  
+    /**
+     *@return false when the board is not solveable and leaves the board filled with zeros; 
+     *        true when the board is solveable, and leaves the board in a solved state
+     *@throws IllegalStateException when the board starts with any non-zero value
+     */
+    public boolean solve(){
+	return true;
+    }
+  
+    /**
+     *@return the number of solutions found, and leaves the board filled with only 0's
+     *@throws IllegalStateException when the board starts with any non-zero value
+     */
+    public int countSolutions(){
+	return 0;
+    }
+  
+
+}
