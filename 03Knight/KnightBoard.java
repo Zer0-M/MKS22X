814d0eb0c7be97e0c22619250d3a52e730702ef4
Zer0-M <msj2340@gmail.com>
2018-02-13 14:29:54 -0500

    added method signatures with placeholder return values

---
+++ 03Knight/KnightBoard.java
@@ -0,0 +1,21 @@
+public class KnightBoard{
+    int[][] board
+    public KnightBoard(int startingRows,int startingCols){
+	board=new int[startingRows,startingCols];
+    }
+    //non square is required!
+
+    public String toString(){
+	return "";
+    }
+
+    public boolean solve(){
+	return false;
+    }
+    public int countSolutions(){
+	return 0;
+    }
+    private boolean solveH(int row ,int col, int level){
+	return false;
+    }
+}
