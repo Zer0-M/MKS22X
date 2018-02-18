b9fa26718f2199380ae4f1896e0acbd705a2ab6a
Zer0-M <mjamil90@stuy.edu>
2018-02-18 13:14:32 -0500

    solve is partially working; it can solve up to 7x6 board

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class KnightBoard{
-    int[][] board;
+    private static int[][] board;
     public KnightBoard(int startingRows,int startingCols){
 	board=new int[startingRows][startingCols];
     }
@@ -21,12 +21,12 @@ public class KnightBoard{
       return Board;
     
     }
-    private int[][] possibleMoves(int r, int c){
+    private static int[][] possibleMoves(int r, int c){
 	int[][] sols=new int[8][2];
 	int x=r;
 	int y=c;
 	int count=0;
-	int incR=3;
+	int incR=2;
 	int incC=1;
 	for(int i=0;i<8;i++){
 	    if(i==1||i==5){
@@ -35,41 +35,50 @@ public class KnightBoard{
 	    if(i==2||i==6){
 		incR=0-incR;
 	    }
-	    if(i==3&i==7){
+	    if(i==3||i==7){
 		incC=0-incC;
 	    }
 	    if(i==4){
-		incC=3;
+		incC=2;
 		incR=1;
 	    }
-	    sols[count][0]=r+incR;
-	    sols[count][1]=c+incC;
+      if(r+incR<board.length&&c+incC<board[0].length&&c+incC>=0&&r+incR>=0&&board[r+incR][c+incC]==0){
+        sols[count][0]=r+incR;
+        sols[count][1]=c+incC;
+      }
 	    count+=1;
-	}
+  }
 	return sols;
     }
-    public boolean solve(){
-	int midR=board.length/2;
-	int midC=board[0].length/2;
-	return solver(midR,midC,1);
+    public boolean solve(){      
+      return solver(0,0,1);
     }
     public int countSolutions(){
 	return 0;
     }
-    private boolean solver(int row ,int col, int level){
-	if(level>=board.length*board[0].length){
+    private boolean solver(int r ,int c, int level){
+  if(level==board.length*board[0].length){
+    board[r][c]=level;
 	    return true;
 	}
+  for(int i=0;i<possibleMoves(r,c).length;i++){
+    if(possibleMoves(r,c)[i][0]!=0||possibleMoves(r,c)[i][1]!=0){
+      board[r][c]=level;
+      if(solver(possibleMoves(r,c)[i][0],possibleMoves(r,c)[i][1],level+1)){
+        return true;
+      }
+      else{
+        board[r][c]=0;
+      }
+    }
+  }
 	return false;
     }
     public static void main(String[] args){
-	KnightBoard k= new KnightBoard(5,5);
-	for(int i=0;i<k.possibleMoves(3,3).length;i++){
-	    board[possibleMoves(3,3)[i][0]][possibleMoves(3,3)[i][1]]==1;
-	}
+      KnightBoard k= new KnightBoard(7,6);
 	System.out.println(k);
-	System.out.println(Arrays.deepToString(k.possibleMoves(3,3)));
-	k.solve();
+	System.out.println(Arrays.deepToString(k.possibleMoves(0,0)));
+	System.out.println(k.solve());
 	System.out.println(k);
     }
 }
