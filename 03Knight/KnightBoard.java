78e364b0878c73c96706c508a60e6bc925b4d45b
Zer0-M <mjamil90@stuy.edu>
2018-02-19 20:42:41 -0500

    countSolutons working for boards up to 5x5; fixed methods according to requirements

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,84 +1,109 @@
 import java.util.*;
 public class KnightBoard{
-    private static int[][] board;
-    public KnightBoard(int startingRows,int startingCols){
-	board=new int[startingRows][startingCols];
-    }
+  private static int[][] board;
+  public KnightBoard(int startingRows,int startingCols){
+    board=new int[startingRows][startingCols];
+  }
 
-    public String toString(){
-      String Board="";
-      for(int i=0;i<board.length;i++){
-        for(int j=0;j<board[i].length;j++){
-          if(board[i][j]<10){
-            Board+=" "+board[i][j]+" ";
-          }
-          else{
-            Board+=board[i][j]+" ";
-          }
+  public String toString(){
+    String Board="";
+    for(int i=0;i<board.length;i++){
+      for(int j=0;j<board[i].length;j++){
+        if(board[i][j]<10){
+          Board+=" "+board[i][j]+" ";
         }
-        Board+="\n";
+        else{
+          Board+=board[i][j]+" ";
+        }
       }
-      return Board;
-    
+      Board+="\n";
     }
-    private static int[][] possibleMoves(int r, int c){
-	int[][] sols=new int[8][2];
-	int x=r;
-	int y=c;
-	int count=0;
-	int incR=2;
-	int incC=1;
-	for(int i=0;i<8;i++){
+    return Board;
+    
+  }
+  private static int[][] possibleMoves(int r, int c){
+    int[][] sols=new int[8][2];
+    int x=r;
+    int y=c;
+    int count=0;
+    int incR=2;
+    int incC=1;
+    for(int i=0;i<8;i++){
 	    if(i==1||i==5){
-		incC=0-incC;
+        incC=0-incC;
 	    }
 	    if(i==2||i==6){
-		incR=0-incR;
+        incR=0-incR;
 	    }
 	    if(i==3||i==7){
-		incC=0-incC;
+        incC=0-incC;
 	    }
 	    if(i==4){
-		incC=2;
-		incR=1;
+        incC=2;
+        incR=1;
 	    }
       if(r+incR<board.length&&c+incC<board[0].length&&c+incC>=0&&r+incR>=0&&board[r+incR][c+incC]==0){
         sols[count][0]=r+incR;
         sols[count][1]=c+incC;
       }
 	    count+=1;
+    }
+    return sols;
   }
-	return sols;
-    }
-    public boolean solve(){      
-      return solver(0,0,1);
-    }
-    public int countSolutions(){
-	return 0;
-    }
-    private boolean solver(int r ,int c, int level){
-  if(level==board.length*board[0].length){
-    board[r][c]=level;
-	    return true;
-	}
-  for(int i=0;i<possibleMoves(r,c).length;i++){
-    if(possibleMoves(r,c)[i][0]!=0||possibleMoves(r,c)[i][1]!=0){
+  /**
+    @throws IllegalStateException when the board contains non-zero values.
+    @throws IllegalArgumentException when either parameter is negative 
+    or out of bounds.
+  */
+  public boolean solve(int startingRow, int startingCol){
+    return solver(startingRow,startingCol,1);
+  }
+
+
+  /**@throws IllegalStateException when the board contains non-zero values. 
+     @throws IllegalArgumentException when either parameter is negative 
+     or out of bounds.
+  */
+  public int countSolutions(int startingRow, int startingCol){
+    return counter(startingRow,startingCol,1);
+  }
+  private boolean solver(int r ,int c, int level){
+    if(level==board.length*board[0].length){
       board[r][c]=level;
-      if(solver(possibleMoves(r,c)[i][0],possibleMoves(r,c)[i][1],level+1)){
-        return true;
+	    return true;
+    }
+    for(int i=0;i<possibleMoves(r,c).length;i++){
+      if(possibleMoves(r,c)[i][0]!=0||possibleMoves(r,c)[i][1]!=0){
+        board[r][c]=level;
+        if(solver(possibleMoves(r,c)[i][0],possibleMoves(r,c)[i][1],level+1)){
+          return true;
+        }
+        else{
+          board[r][c]=0;
+        }
       }
-      else{
+    }
+    return false;
+  }
+    private int counter(int r ,int c, int level){
+      int sum=0;
+    if(level>=board.length*board[0].length){
+	    return 1;
+    }
+    for(int i=0;i<possibleMoves(r,c).length;i++){
+      if(possibleMoves(r,c)[i][0]!=0||possibleMoves(r,c)[i][1]!=0){
+        board[r][c]=level;
+        sum+=counter(possibleMoves(r,c)[i][0],possibleMoves(r,c)[i][1],level+1);
         board[r][c]=0;
       }
     }
+    return sum;
   }
-	return false;
-    }
-    public static void main(String[] args){
-      KnightBoard k= new KnightBoard(7,6);
-	System.out.println(k);
-	System.out.println(Arrays.deepToString(k.possibleMoves(0,0)));
-	System.out.println(k.solve());
-	System.out.println(k);
-    }
+  public static void main(String[] args){
+    KnightBoard k= new KnightBoard(6,6);
+    System.out.println(k);
+    System.out.println(Arrays.deepToString(k.possibleMoves(0,0)));
+    System.out.println(k.countSolutions(0,0));
+    System.out.println(k);
+  }
 }
