eee76696ce4a8d16a247e64adef22652284774ce
Mohammed S. Jamil <mjamil90@stuy.edu>
2018-03-09 23:22:45 -0500

    bronze now works; tested with all cases from USACO website

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -4,10 +4,8 @@ import java.util.*;
 public class USACO{
     private static int[][] map;
     private static int[][] instructions;
-    private static boolean stompMap;
 
     public static int bronze(String filename){
-        stompMap=true;
         int R=0;
         int C=0;
         int E=0;
@@ -45,34 +43,41 @@ public class USACO{
             stomp(i);
         }
         fill(E);
-        System.out.println(R + " "+C +" "+ E+" "+N+" "+Arrays.deepToString(map)+" "+Arrays.deepToString(instructions));
+        int combinedDepth=0;
+        for(int i=0;i<map.length;i++){
+            for(int j=0;j<map[i].length;j++){
+                if(E-map[i][j]>0){
+                    combinedDepth+=map[i][j];
+                }
+            }
+        }
+        
+        return combinedDepth*72*72;
         }
         catch(FileNotFoundException e){
             System.out.println("please provide vaild file");
             System.exit(0);
         }
-        return 0;    
+        return 0;
     }
     private static void stomp(int i){
         int R_s=instructions[i][0]-1;
         int C_s=instructions[i][1]-1;
         int peak=map[R_s][C_s]; //since the cows wait for cows at higher level are on the same level
         for(int ind=R_s;ind<R_s+3&&ind<map.length;ind++){
-            for(int j=C_s;j<C_s+3&&j<map[i].length;j++){
+            for(int j=C_s;j<C_s+3&&j<map[ind].length;j++){
                 if(map[ind][j]>peak){
                     peak=map[ind][j];
                 }
             }
         }
-        System.out.println(R_s+" "+C_s);
+        
         stompH(i,R_s,C_s,peak);
     }
     private static void stompH(int i,int r, int c,int peak){
-        int R_s=instructions[i][0]-1;
-        int C_s=instructions[i][1]-1;
         int D_s=instructions[i][2];
-        for(int ind=R_s;ind<R_s+3&&ind<map.length;ind++){
-            for(int j=C_s;j<C_s+3&&j<map[i].length;j++){
+        for(int ind=r;ind<r+3&&ind<map.length;ind++){
+            for(int j=c;j<c+3&&j<map[ind].length;j++){
                 if(map[ind][j]-(peak-D_s)<=D_s&&map[ind][j]-(peak-D_s)>=0){
                     map[ind][j]=peak-D_s;
                 }
@@ -96,7 +101,11 @@ public class USACO{
 	return 0;
     }
     public static void main(String[] args){
-        System.out.println(USACO.bronze("makelake.in"));
+        for(int i=1;i<11;i++){
+        int sol=USACO.bronze("makelake."+i+".in");
+        int [] sols={342144,102762432,1058992704,753121152,1028282688,72207936,265508928,776609856,2098830528,211201344};
+        System.out.println(sol==sols[i-1]);
+        }
     }
 
 }
