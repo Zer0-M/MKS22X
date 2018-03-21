39b60a2e1254d1ffcaf8baa5a9f71bdc452c2e8a
Mohammed S. Jamil <mjamil90@stuy.edu>
2018-03-21 12:54:56 -0400

    implemented the better mergesort algorithm Mr. K showed in class

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,46 +1,47 @@
 import java.util.Arrays;
 
 public class Merge{
-    private static void merge(int[] data,int lo,int hi){
+    private static void merge(int[] data,int[] temp,int lo,int hi){
         int mid=lo+(hi-lo)/2;
-        int[] temp=new int[data.length];
+
         int tempCounter=0;
 		for(int i=lo,j=mid+1;i<=mid||j<=hi;){
             if(j>hi){
-                temp[lo+tempCounter]=data[i];
+                data[lo+tempCounter]=temp[i];
                 tempCounter++;
                 i++;
             }
             else if(i>mid){
-                temp[lo+tempCounter]=data[j];
+                data[lo+tempCounter]=temp[j];
                 tempCounter++;
                 j++;
             }
-			else if(data[i]<data[j]){
-                temp[lo+tempCounter]=data[i];
+			else if(temp[i]<temp[j]){
+                data[lo+tempCounter]=temp[i];
                 tempCounter++;
                 i++;
             }
             else{
-                temp[lo+tempCounter]=data[j];
+                data[lo+tempCounter]=temp[j];
                 tempCounter++;
                 j++;
             }
 
         }
-        for(int i=lo;i<=hi;i++){
-            data[i]=temp[i];
-        }
     }
     public static void mergesort(int[] data){
-		mergesortH(data, 0, data.length-1);
+        int[] temp=new int[data.length];
+        for(int i=0;i<data.length;i++){
+            temp[i]=data[i];
+        }
+		mergesortH(data,temp, 0, data.length-1);
     }
-    private static void mergesortH(int[] data,int lo,int hi){
+    private static void mergesortH(int[] data,int[] temp,int lo,int hi){
         if(lo<hi){
             int mid=lo+(hi-lo)/2;
-            mergesortH(data, lo, mid);
-            mergesortH(data, mid+1, hi);
-            merge(data,lo,hi);
+            mergesortH(temp,data, lo, mid);
+            mergesortH(temp,data, mid+1, hi);
+            merge(data,temp,lo,hi);
         }
     }
 }
