ede7c98f5fa9f59d2686a11de8f0d6f854ee7617
Mohammed S. Jamil <mjamil90@stuy.edu>
2018-03-21 13:17:44 -0400

    added Quick.java to compare runtimes; added runtime test to driver

--- 07Merge/Driver.java
+++ 07Merge/Driver.java
@@ -1,21 +1,48 @@
 import java.util.*;
 public class Driver{
     public static void main(String[] args){
+        int winD=0;
+        int win=0;
+        int winM=0;
         for(int j=0;j<1000;j++){
             Random rand=new Random();
             int length= rand.nextInt(1000);
             int[] arr=new int[length];
             int[] ary=new int[length];
+            int[] ar=new int[length];
+            int[] data=new int[length];
             for(int i=0;i<length;i++){
-                int r=rand.nextInt(rand.nextInt(100)+1);
+                int r=rand.nextInt(rand.nextInt(1000)+1);
                 if(i%5==0){
                     r=0-r;
                 }
                 arr[i]=r;
                 ary[i]=r;
+                ar[i]=r;
+                data[i]=r;
             }
+            long start,end;
+            start = System.nanoTime();
             Merge.mergesort(arr);
-
+            end = System.nanoTime();
+            long elapsedM=end-start;
+            start = System.nanoTime();
+            Quick.quicksort(data);
+            end = System.nanoTime();
+            long elapsedD=end-start;
+            start = System.nanoTime();
+            Quick.quicksortOld(ar);
+            end = System.nanoTime();
+            long elapsed=end-start;
+            if(Math.max(elapsedM,Math.max(elapsedD,elapsed))==elapsedM){
+                winM++;
+            }
+            if(Math.max(elapsedM,Math.max(elapsedD,elapsed))==elapsed){
+               win++;
+            }
+            if(Math.max(elapsedM,Math.max(elapsedD,elapsed))==elapsedD){
+               winD++;
+            }
             // startTime=System.currentTimeMillis();
             Arrays.sort(ary);
             // stopTime = System.currentTimeMillis();
@@ -26,5 +53,7 @@ public class Driver{
                 System.out.println(Arrays.toString(arr));
              }
         }
+
+        System.out.println("QuickSort Regular: "+win+" vs QuickSort Dutch: "+winD+" vs MergeSort: "+winM);
     }
 }
---
+++ 07Merge/Quick.java
@@ -0,0 +1,100 @@
+import java.util.*;
+
+public class Quick{
+    public static void swap(int[]data, int a, int b){
+        int tem=data[a];
+        data[a]=data[b];
+        data[b]=tem;
+    }
+    /*Choose a random pivot element, and modify the array such that:
+    *1. Only the indices from start to end inclusive are considered in range
+    *2. A random index from start to end inclusive is chosen, the corresponding 
+    *   element is designated the pivot element. 
+    *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
+    *4. all elements in range that are larger than the pivot element are placed after the pivot element.
+    *@return the index of the final position of the pivot element.
+    */
+    static int partition0 ( int [] data, int start, int end){
+        Random rand=new Random();
+        if(start==end){
+            return data[start];
+	}
+        int pivotI=start+rand.nextInt(end-start);
+        swap(data,pivotI,start);
+        int small=start+1;
+        int large=end;
+        while(small<=large){
+            if(data[small]>=data[start]){
+                swap(data,large,small);
+                large--;
+            }
+            else {
+                small++;
+            }
+        }
+        swap(data,start,large);
+        
+        return large;
+    }
+    static int[] partition ( int [] data, int start, int end){
+        Random rand=new Random();
+        int pivotI=start+rand.nextInt(end-start);
+        int pivot=data[pivotI];
+        int lt=start;
+        int gt=end;
+        int i=start;
+        while(i<=gt){
+            if(data[i]==pivot){
+                i++;
+            }
+            else if(data[i]>pivot){
+                swap(data,i,gt);
+                gt--;
+            }
+            else if(data[i]<pivot){
+                swap(data,i,lt);
+                lt++;
+                i++;
+            }
+        }
+        int[] part=new int[2];
+        part[0]=lt-1;
+        part[1]=i;
+        return part;
+    }
+    public static int quickselect(int[] data, int k){
+        int start=0;
+        int end=data.length-1;
+        int part=partition0(data,start,end);
+        while(start<end&&k!=part){ 
+            if(part>k){
+                end=part-1;
+            }
+            else if(part<k){
+                start=part+1;
+            }
+            part=partition0(data,start,end);
+        }
+        return data[k];
+    }
+    public static void quicksort(int[] data){
+        sortHelper(data, 0,data.length-1 );
+    }
+    private static void sortHelper(int[] data,int start, int end){
+        if(start<end){
+            int[] part=partition(data,start,end);
+            sortHelper(data,start,part[0]);
+            sortHelper(data,part[1],end);
+        }
+    }
+    public static void quicksortOld(int[] data){
+        sortHelperOld(data, 0,data.length-1 );
+    }
+    private static void sortHelperOld(int[] data,int start, int end){
+        if(start<end){
+            int part=partition0(data,start,end);
+            sortHelperOld(data,start,part-1);
+            sortHelperOld(data,part+1,end);
+        }
+    }
+}
