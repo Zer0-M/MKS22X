7b7a63d1cb0e9d5d53f1719f5b38c54ae93d7c45
Mohammed S. Jamil <mjamil90@stuy.edu>
2018-03-27 23:36:49 -0400

    tested methods in Node

--- 08LinkedList/MyLinkedList.java
+++ 08LinkedList/MyLinkedList.java
@@ -1,27 +1,30 @@
 public class MyLinkedList{
-    private class Node{
+    public class Node{
         Node next,prev;
         int data;
         public Node(int dat){
             data=dat;
         }
+        public String toString(){
+            return "["+data+"]";
+        }
         public Node getNext(){
             return next;
         }
         public void setNext(Node n){
-            
+            next=n;
         }
         public Node getPrev(){
             return prev;
         }
         public void setPrev(Node n){
-            
+         	prev=n;  
         }
         public int getValue(){
 			return data;
         }
         public void setValue(int n){
-
+			data=n;
         }
     }
 }
