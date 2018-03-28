a443fa0e347452a8276d189895ffaca9ef4974d4
Mohammed S. Jamil <mjamil90@stuy.edu>
2018-03-28 00:53:36 -0400

    adding from the front tested; toString tested

--- 08LinkedList/MyLinkedList.java
+++ 08LinkedList/MyLinkedList.java
@@ -2,17 +2,47 @@ public class MyLinkedList{
     private Node first;
     private Node last;
     private int length;
-    public MyLinkedList(int len){
-
+    public MyLinkedList(){
+        length=0;
     }
     public boolean add(int value){
+        Node addend=new Node(value);
+        /*adding from the front
+        if(first==null){
+            first=addend;
+            last=first;
+        }
+        else{
+            Node temp=new Node(first.getValue());
+            temp.setNext(first.getNext());
+            temp.setPrev(first.getPrev());
+            first.setPrev(addend);
+            first=first.getPrev();
+            first.setNext(temp);
+        }*/
+        length++;
+        //last.setNext(addend);
+
         return true;
     }
     public int size(){
         return length;
     }
+
     public String toString(){
-        return"";
+        Node n=first;
+        String s="[";
+        for(int i=0;i<length;i++){
+            if(n!=null){
+            s+=n.getValue();
+            }
+            if(n.hasNext()){
+                s+=",";
+                n=n.getNext();
+            }
+        }
+        s+="]";
+        return s;
     }
     public int get(int index){
 		return 0;
@@ -49,5 +79,17 @@ public class MyLinkedList{
         public void setValue(int n){
 			data=n;
         }
+        public boolean hasNext(){
+            if(getNext()==null){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+    }
+    public static void main(String[] args){
+        MyLinkedList L=new MyLinkedList();
+        System.out.println(L);
     }
 }
