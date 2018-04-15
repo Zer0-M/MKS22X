d54ddc0e99aaa59e17d4d2259bcb90b8c9f26c86
Mohammed S. Jamil <mjamil90@stuy.edu>
2018-04-14 21:50:09 -0400

    radixsort works for positive and negative numbers

--- 08LinkedList/Sorts.java
+++ 08LinkedList/Sorts.java
@@ -9,54 +9,76 @@ public class Sorts{
 		return num.toString().length();
 	}
 	public static void radixsort(MyLinkedListImproved<Integer> data){
-		int max=data.max();
+		MyLinkedListImproved<Integer> absoluteData=new MyLinkedListImproved<>();
+		for (Integer i:data){
+			if(i<0){
+				i=0-i;
+			}
+			absoluteData.add(i);
+		}
+		int max=absoluteData.max();
+		if(max>=0){
 		int maxdig=countDigits(data.get(max));
 		for(int j=1;j<=maxdig;j++){
 			@SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] arr=new MyLinkedListImproved[10];
-			//System.out.println(Arrays.toString(arr));
+			@SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] neg=new MyLinkedListImproved[10];
+			
 			for(int i=0;i<arr.length;i++){
 				arr[i]=new MyLinkedListImproved<Integer>();
+				neg[i]=new MyLinkedListImproved<Integer>();
 			}
 			for(Integer v:data){
 				int i=getDigit(j, v);
-				//System.out.println(arr[i]);
-				arr[i].add(v);
+				if(v<0){
+					neg[9+i].add(v);
+				}
+				else{
+					arr[i].add(v);
+				}
 			}
 			data.clear();
+			for(int i=0;i<neg.length;i++){
+				data.extend(neg[i]);
+			}
 			for(int i=0;i<arr.length;i++){
 				data.extend(arr[i]);
-				//System.out.println("L: "+L);
 			}
 		}
-		
+	}
 	}
 	public static void main(String[] args){
 		MyLinkedListImproved<Integer> L=new MyLinkedListImproved<>();
 		Random rand=new Random();
 		int[] arr=new int[1000000];
-		String s="";
-		long end,start=System.currentTimeMillis();
+		
 		for(int i=0;i<1000000;i++){
-			int value=rand.nextInt(2147483647);
-			char c=(char)value;
-			//if(i%5==0){
-			L.add(value);
-			arr[i]=value;
-			//LL.add(value*rand.nextInt(10));
-			//	s="";
+			int value=rand.nextInt();
+			//if(i%2!=0){
+				L.add(value);
+				arr[i]=value;
 			//}
-			//s+=c;
+			//else{
+			//L.add(value);
+			//arr[i]=value;
+			//}
+			
 		}
-		end=System.currentTimeMillis();
+		
+		
+		long end,start=System.currentTimeMillis();
 		radixsort(L);
-		System.out.println((end-start));
+		end=System.currentTimeMillis();
+		System.out.println(((float)(end-start))/1000);
+		//Arrays.sort(arr);
 		int i=0;
-		Arrays.sort(arr);
-		for(Integer v:L){
+		
+		/*for(Integer v:L){
 			if(v!=arr[i]){
 				System.out.println("not sorted");
+				System.out.println(i+" "+v+" "+arr[i]);
+				System.exit(0);
 			}
 			i++;
-		}
+		}*/
 	}
 }
