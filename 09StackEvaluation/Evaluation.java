9d18484d5f0130149248de15f19bc2280b31465e
Zer0-M <msj2340@gmail.com>
2018-04-17 14:50:06 -0400

    added files for stack evaluation

---
+++ 09StackEvaluation/Evaluation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Evaluation{
+    public static double eval(String s){
+	return 0.0;
+    }
+    public static void main(String[] arg){
+	
+    }
+
+}
---
+++ 09StackEvaluation/Stack.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Stack<T>{
+    LinkedList<T> L;
+    public Stack(){
+	L=new LinkedList<T>();
+    }
+    public static void push(e){
+
+    }
+    public static e pop(){
+
+    }
+    
+}
