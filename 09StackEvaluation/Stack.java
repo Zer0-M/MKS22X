142db764108b58fae6897c8f52d030b5d09c0c06
Mohammed S. Jamil <mjamil90@stuy.edu>
2018-04-17 23:23:29 -0400

    stack class working

--- 09StackEvaluation/Stack.java
+++ 09StackEvaluation/Stack.java
@@ -1,14 +1,35 @@
 import java.util.*;
 public class Stack<T>{
     LinkedList<T> L;
+    
     public Stack(){
-	L=new LinkedList<T>();
+		L=new LinkedList<T>();
     }
-    public static void push(e){
-
+    public String toString(){
+        return L.toString();
     }
-    public static e pop(){
-
+    public void push(T e){
+		L.add(e);
+    }
+    public T pop(){
+        return L.remove(L.size()-1);
+        
+    }
+    public static void main(String[] args){
+        Stack<Integer> S= new Stack<>();
+        S.push(13);
+        S.push(12);
+        S.push(2);
+        S.push(-131);
+        S.push(8970708);
+        System.out.println(S);
+        System.out.println(S.pop());
+        System.out.println(S.pop());
+        System.out.println(S.pop());
+        System.out.println(S.pop());
+        System.out.println(S.pop());
+        System.out.println(S);
+        //System.out.println(S.pop());
     }
     
 }
