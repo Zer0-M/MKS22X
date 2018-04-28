ee52859adc810f0d90155824ee419a1ac1346de8
Mohammed S. Jamil <mjamil90@stuy.edu>
2018-04-28 01:54:38 -0400

    add for max heap working

---
+++ 12Heap/MyHeap.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class MyHeap{
+    String[] data;
+    int length;
+    boolean isMax;
+    public MyHeap(){
+        data=new String[13];
+        isMax=true;
+    }
+    public String toString(){
+        return Arrays.toString(data);
+    }
+    private void swap(String[] arr,int i,int j){
+        String temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public int size(){
+        return length;
+    }
+    public void add(String s){
+        data[length]=s;
+        int j=length;
+        if(length>0){
+            if(isMax){
+                for(int i=(length-1)/2;s.compareTo(data[i])>0;i=(i-1)/2){
+                    //System.out.println(i+" "+s.compareTo(data[i]));
+                    //System.out.println(s+" "+data[i]);
+                    swap(data,i,j);
+                    j=i;
+                }
+            }
+        }
+        length++;
+        
+    }
+    public static void main(String[] args){
+        MyHeap h=new MyHeap();
+        System.out.println(h);
+    }
+}
