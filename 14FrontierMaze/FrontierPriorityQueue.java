c8d436fcce9b04b83713ed87ec30a48d4d33a1eb
Mohammed S. Jamil <mjamil90@stuy.edu>
2018-05-08 20:10:11 -0400

    removed priority queue solve because it doesn't work

--- 14FrontierMaze/FrontierPriorityQueue.java
+++ 14FrontierMaze/FrontierPriorityQueue.java
@@ -15,4 +15,16 @@ public class FrontierPriorityQueue implements Frontier{
     public boolean hasNext(){
         return priority.peek()!=null;
     }
+    public static void main(String[] args){
+        Location a=new Location(0,0,null);
+        Location b=new Location(3,4,null);
+        Location c=new Location(5,6,null);
+        Location d=new Location(2,1,null);
+        Location e=new Location(10,0,null);
+        Location f=new Location(34,20,null);
+        FrontierPriorityQueue q=new FrontierPriorityQueue();
+        q.add(a);
+        q.add(b);
+        System.out.println(q);
+    }
 }
